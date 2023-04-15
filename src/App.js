8169eb5256b9b95411b05716807e772bc8aea65a
Abhay Rana <abhayrana@Abhays-MacBook-Air.local>
2023-04-15 13:56:54 +0530

    ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import ImgUpload from './components/ImgUpload.component';
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <div className="card">
+              <div className="card-header">
+                Upload Image
+              </div>
+              <div className="card-body">
+                <ImgUpload />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
-
 export default App;
